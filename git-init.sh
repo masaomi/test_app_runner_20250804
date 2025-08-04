383f1b2eae4ed4718e6a1a0eb1a7562356cd16cd
masaomi <masaomi.hatakeyama@gmail.com>
2025-08-04 15:11:10 +0200

    command.log

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+echo "# test_app_runner_20250804" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:masaomi/test_app_runner_20250804.git
+git push -u origin main
