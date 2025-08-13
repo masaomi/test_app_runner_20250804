1b8841e411cd2771c57317c12e603c428fa7a84f
masaomi <masaomi.hatakeyama@gmail.com>
2025-08-13 14:40:39 +0200

    Added root page and fixed Dockerfile to use 8080

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  Rails.application.routes.draw do
+    root to: proc { [200, {}, ["Hello, World!"]] }
+  end
+
 end
